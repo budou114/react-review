4613fe9918331b918f5ea5716636755f238f042d
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-12 15:15:32 +0900

    擬似的にレンダリングコストの高いコンポーネントを作成

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -4,9 +4,15 @@ const style = {
 	backgroundColor: 'khaki'
 }
 
-
 export const ChildArea = (props) => {
 	const { open } = props;
+	console.log('ChildAreaがレンダリングされた!!');
+
+	const data = [...Array(2000).keys()];
+	data.forEach(() => {
+		console.log('...')
+	});
+
 	return (
 		<>
 			{open ? (
