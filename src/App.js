59308a1a35ee1a47ce5b02684409648201c9c2b1
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-12 14:56:34 +0900

    レンダリング確認用のコーディング

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import { ChildArea } from './ChildArea';
 
 export default function App() {
   console.log('App');
-  
+
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
 
   const onClickCountUp = () => {
     setCount(count + 1);
   };
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className='App'>
@@ -16,6 +23,14 @@ export default function App() {
       <h2>test</h2>
       <p>{count}</p>
       <button onClick={onClickCountUp}>カウントアップ</button>
+
+      <div>
+        <input value={text} onChange={onChangeText}/>
+        <br />
+        <br />
+        <button onClick={onClickOpen}>表示</button>
+        <ChildArea open={open}/>
+      </div>
     </div>
   );
 }
